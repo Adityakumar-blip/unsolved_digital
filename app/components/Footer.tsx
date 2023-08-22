e8685e198a4d50403a30460bc9556daed1f7f72d
unknown <adityakamal303@gmail.com>
2023-08-22 23:25:46 +0530

    layout fixed

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='w-full bg-white p-3 text-black font-bold fixed bottom-0'>Footer</div>
+    )
+}
+
+export default Footer
---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='w-full bg-white p-3 text-black font-bold'>Header</div>
+    )
+}
+
+export default Header
