c8a9f0fd2669bcb1a23c73db33bbdf0147457ae8
unknown <adityakamal303@gmail.com>
2023-08-23 22:30:55 +0530

    layout implemented

---
+++ app/utils/ImageService.ts
@@ -0,0 +1,6 @@
+
+import UnsolvedLogo from '@/app/assets/images/unsolved-logo.svg'
+
+export const Images = {
+    UnsolvedLogo
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from 'tailwindcss'
 
+const primaryText = "#641BA9";
+const secondaryText = "#FFF";
+const accentText = "#494949";
+
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -13,8 +18,31 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary': primaryText,
+        'secondary': secondaryText,
+        'accent': accentText,
+      },
+      backgroundColor: {
+        'primary': "black"
+      },
+      fontSize: {
+        '8xl': '96px',
+        '7xl': "64px",
+        '6xl': '54px',
+        '5xl': '48px',
+        '4xl': '36px',
+        '3xl': '32px',
+        '2xl': '30px',
+        'xl': '24px',
+        'lg': '20px',
+        'base': '16px',
+        'sm': '15px',
+        'xs': '14px',
+      }
     },
   },
   plugins: [],
+  important:true
 }
 export default config
